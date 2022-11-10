8235293048529eaeaa1956d0575e3ebc18299dd2
Merdasa-Belay <merdasabelay@gmail.com>
2022-11-10 18:19:23 +0300

    deleting

---
+++ python-daily_exercise/turtle_race/practice.py
@@ -0,0 +1,27 @@
+# class Animal:
+#     def __init__(self):
+#         self.num_eyes = 2
+
+#     def breath(self):
+#         print("Inhale and Exhale")
+
+
+# class Fish(Animal):
+#     def __init__(self):
+#         super().__init__()
+
+#     def breath(self):
+#         super().breath()
+#         print("Doing this under water")
+
+#     def swim(self):
+#         print("moving in water.")
+
+
+# nemo = Fish()
+# nemo.swim()
+# nemo.breath()
+# print(nemo.num_eyes)
+piano_key = ["a", "b", "c", "d", "e", "f", "g", "h"]
+# print(piano_key[2:7:2])
+print(piano_key[::1])
