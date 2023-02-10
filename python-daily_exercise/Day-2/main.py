27a369bdf5b047b7b25d4001a2cb71d47dfabb9e
Merdasa-Belay <merdasabelay@gmail.com>
2023-02-10 15:34:13 +0300

    age calculator

---
+++ python-daily_exercise/Day-2/main.py
@@ -0,0 +1,8 @@
+age = int(input("What is your current age? "))
+
+left_age = 90 - age
+days = left_age * 365
+weeks = left_age * 52
+months = left_age * 12
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left")
