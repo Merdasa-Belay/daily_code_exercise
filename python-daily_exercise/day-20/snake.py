31635638b3dc6d54ddc5f0e08184ed2a38931319
Merdasa-Belay <merdasabelay@gmail.com>
2023-06-25 09:54:34 +0300

    snake segment movment on keyboard

---
+++ python-daily_exercise/day-20/snake.py
@@ -0,0 +1,47 @@
+from turtle import Turtle, Screen
+
+SEGMENT_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.snake_index = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in SEGMENT_POSITIONS:
+            self.snake = Turtle()
+            self.snake.color("white")
+            self.snake.shape("square")
+            self.snake.penup()
+            self.snake.goto(position)
+            self.snake_index.append(self.snake)
+
+    def move(self):
+        for seg in range(len(self.snake_index) - 1, 0, -1):
+            new_x = self.snake_index[seg - 1].xcor()
+            new_y = self.snake_index[seg - 1].ycor()
+            self.snake_index[seg].goto(new_x, new_y)
+
+        self.snake_index[0].forward(20)
+
+    def up(self):
+        if self.snake_index[0].heading() != DOWN:
+            self.snake_index[0].setheading(UP)
+
+    def down(self):
+        if self.snake_index[0].heading() != UP:
+            self.snake_index[0].setheading(DOWN)
+
+    def right(self):
+        if self.snake_index[0].heading() != LEFT:
+            self.snake_index[0].setheading(RIGHT)
+
+    def left(self):
+        if self.snake_index[0].heading() != RIGHT:
+            self.snake_index[0].setheading(LEFT)
