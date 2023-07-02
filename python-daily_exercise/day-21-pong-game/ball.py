eeba0ed85b7d3c2162c12d244bfa394013352f6d
Merdasa-Belay <merdasabelay@gmail.com>
2023-07-02 19:23:30 +0300

    Detect collision with the wall to bounce

--- python-daily_exercise/day-21-pong-game/ball.py
+++ python-daily_exercise/day-21-pong-game/ball.py
@@ -1,13 +1,24 @@
 from turtle import Turtle, Screen
 
 
-
 class Ball(Turtle):
-  def __init__(self):
-    super().__init__()
-    self.create_ball()
-  def create_ball(self):
-    self.shape("square")
-    self.shapesize(stretch_wid=1, stretch_len=1)
-    self.color("white")
-    
+    def __init__(self):
+        super().__init__()
+        self.create_ball()
+        self.x_move = 10
+        self.y_move = 10
+
+    def create_ball(self):
+        self.shape("circle")
+        self.shapesize(stretch_wid=1, stretch_len=1)
+        self.color("white")
+        self.penup()
+        self.speed("slow")
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.y_move *= -1
--- python-daily_exercise/day-21-pong-game/main.py
+++ python-daily_exercise/day-21-pong-game/main.py
@@ -3,6 +3,8 @@ from turtle import Turtle, Screen
 from ball import Ball
 from paddle import Paddle
 
+import time
+
 
 screen = Screen()
 screen.setup(height=600, width=800)
@@ -10,6 +12,7 @@ screen.bgcolor("black")
 screen.title("Pong game")
 screen.tracer(0)
 ball = Ball()
+
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
@@ -20,5 +23,10 @@ screen.onkey(l_paddle.up, "w")
 screen.onkey(l_paddle.down, "s")
 GAME_IS_ON = True
 while GAME_IS_ON:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+    # TODO Detecting collision
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
 screen.exitonclick()
