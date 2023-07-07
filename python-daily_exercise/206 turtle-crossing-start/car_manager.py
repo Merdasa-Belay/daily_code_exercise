f611c86c8fd35041d8c0dac3ef7c132a4928eac5
Merdasa-Belay <merdasabelay@gmail.com>
2023-07-07 05:38:44 +0300

    Crating player and cars to move

--- python-daily_exercise/206 turtle-crossing-start/car_manager.py
+++ python-daily_exercise/206 turtle-crossing-start/car_manager.py
@@ -1,7 +1,24 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager():
+    def __init__(self):
+        self.all_cars = []
+
+    def create(self):
+        car = Turtle()
+        car.color(random.choice(COLORS))
+        car.shape("square")
+        car.shapesize(stretch_len=2, stretch_wid=1)
+        car.penup()
+        car.goto(270, random.randint(-250, 250))
+        self.all_cars.append(car)
+        self.move()
+
+    def move(self):
+        for cars in self.all_cars:
+            cars.backward(STARTING_MOVE_DISTANCE)
