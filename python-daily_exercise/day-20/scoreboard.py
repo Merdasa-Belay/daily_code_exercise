eba7e90afa4a74f2ab81de8518fb965d04b1c427
Merdasa Belay <merdasabelay@gmail.com>
2023-08-28 03:45:37 +0300

    read and write the high score to a file

--- python-daily_exercise/day-20/scoreboard.py
+++ python-daily_exercise/day-20/scoreboard.py
@@ -7,7 +7,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        with open("data.txt") as data:
+        with open("data.txt", mode="r") as data:
             self.high_score = int(data.read())
         self.color("white")
         self.hideturtle()
@@ -34,5 +34,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
